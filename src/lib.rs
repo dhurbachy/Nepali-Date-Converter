be6fdf3f7913ead6114c2d044b0912c98c1034f3
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-02-19 12:24:55 +0545

    feat: update libs and test conversion

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,50 @@
+pub mod utils;
+pub mod types;
+pub mod converter;
+pub mod constant;
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-/// Returns the number of days in a given BS year and month (1-12)
-// pub fn get_days_in_month(year: i32, month: usize) -> Option<u8> {
-//     // 1. Find the year in the dataset
-//     // 2. Return the value at (month - 1) index
-//     BS_CALENDAR
-//         .iter()
-//         .find(|&&(y, _)| y == year)
-//         .and_then(|&(_, months)| {
-//             if month >= 1 && month <= 12 {
-//                 Some(months[month - 1])
-//             } else {
-//                 None // Invalid month
-//             }
-//         })
-// }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
+    use chrono::NaiveDate;
+    use crate::types::{YearData, NepaliDate};
+    use crate::converter::DateConverter;
 
+    // Helper function to setup the converter with some sample data
+    fn setup_converter() -> DateConverter {
+        // 1. Read the JSON file
+        // Ensure the path is correct relative to your cargo root
+        let data_str = fs::read_to_string("data/calendar_data.json")
+            .expect("Could not find calendar_data.json");
+
+        // 2. Parse it into the Vec
+        let calendar_data: Vec<YearData> = serde_json::from_str(&data_str)
+            .expect("JSON format is incorrect");
+
+        // 3. Return the converter with the real data
+        DateConverter::new(calendar_data)
+    }
+
+     #[test]
+    fn test_ad_to_bs_conversion() {
+        let converter = setup_converter();
+        
+        // Example: Convert 2023-11-29 AD (which is 2080-08-13 BS)
+        let english_date = NaiveDate::from_ymd_opt(2023, 11, 29).unwrap();
+        
+        let result = converter.ad_to_bs(english_date);
+        
+        assert!(result.is_some(), "Conversion failed");
+        let nepali = result.unwrap();
+        
+        println!("Converted 2023-11-29 to BS: {}-{}-{}", nepali.year, nepali.month, nepali.day);
+
+    }
     #[test]
     fn it_works() {
         let result = add(2, 2);
