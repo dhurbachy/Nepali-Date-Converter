2c4f45366c48fbcb8526cc1065f6fae2a1db167b
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-02-19 12:23:13 +0545

    feat: update constant for reference starting ad and bs

---
+++ src/constant.rs
@@ -0,0 +1,7 @@
+/// The first year available in your JSON data
+pub const BS_START_YEAR: i32 = 2000;
+
+/// The Gregorian (AD) equivalent of 2000-01-01 BS
+pub const AD_EPOCH_YEAR: i32 = 1943;
+pub const AD_EPOCH_MONTH: u32 = 4;
+pub const AD_EPOCH_DAY: u32 = 13;
